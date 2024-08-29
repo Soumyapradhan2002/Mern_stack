0cad0660cbada4db0831af30045d2f594b615ff1
Soumyashree Pradhan <pradhansoumyashree023@gmail.com>
2024-08-29 14:21:57 +0530

    done

---
+++ Javascript/class-3/data.js
@@ -0,0 +1,26 @@
+"use strict"
+
+console.log = "name";
+var
+
+
+
+
+
+
+
+
+
+
+let name1 = null;
+name1 = "pablo";
+ console.log(name1); //null
+
+ 
+
+
+
+
+
+  let name2; //not defined !== undefined
+ console.log(name2); //undefined
